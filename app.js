8c46972a65c9819a3e56e8d4d9fac8708dcfc3e0
Cesar Contreras <ccontreras@nclcorp.com>
2022-09-17 19:57:55 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!xx'));
+app.get('/', (req, res) => res.send('Hello World!xx3x'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
